866462fb5ad7d700c30902034d2987e3a30b688a
Almir Rogério Camolesi <camolesi@fema.edu.br>
2022-05-06 20:57:14 -0300

    Update conexao.php
    Classe Conexão atualizada e completa

--- conexao.php
+++ conexao.php
@@ -13,10 +13,21 @@
         }
 
         public static function conectar(){
-
-            
+            if (self::$cont==null){
+                try{
+                  //  self::$cont = new PDO(("mysql:host=localhost;dbname=imobi", "root", "");    
+                  self::$cont = new PDO(("mysql:host=" . self::$dbHost .";dbname=" . self::$dbName , self::$dbUser, self::$dbPwd);   
+                }
+                catch (PDOException $e){
+                    die ($e->getmessage());  
+                }
+            }
+            return self::$cont; 
         }
 
+        public static function desconectar() {
+            self::$cont = null; 
+        }
 
     }
 
