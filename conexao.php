fbe494fe433f98103078643d00b9c51c00c51fb6
Almir Rogério Camolesi <camolesi@fema.edu.br>
2022-05-06 20:32:29 -0300

    Update conexao.php
    Conexao parcial

--- conexao.php
+++ conexao.php
@@ -1,34 +1,23 @@
-<?php //conexao.php
-     class Conexao {
-        private static $dbNome = 'imobi'; 
+<?php
+
+    class Conexao{
         private static $dbHost = 'localhost'; 
-        private static $dbUsuario = 'root'; 
-        private static $dbSenha = ''; 
+        private static $dbName = 'imobi'; 
+        private static $dbUser = 'root'; 
+        private static $dbPwd = ''; 
 
         private static $cont = null; 
 
-        public function __construct()
-        {
-            die("Passei pelo Construtor da Classe Conexão"); 
+        public function __construct(){
+            die ("Passei pelo construtor de conexao");
         }
 
         public static function conectar(){
-            if (self::$cont == null){
-                try {       
-                self::$cont = new PDO('mysql:host=localhost;dbname=imobi', self::$dbUsuario, self::$dbSenha); 
-                            
-            }
-                catch(PDOException $exception){
-                     die ($exception->getmessage()); 
-                }
-         
-           }
-           return self::$cont; 
-       }
 
-        public static function desconectar() {
-            self::$cont = null; 
+            
         }
-   }
+
+
+    }
 
 ?>
